efd50a437295ab4d12ed6967a6cbe8f8921625a0
Malin Classon <m.classon@hotmail.com>
2016-04-06 23:57:36 +0200

    Revert "Laggt till stöd för kassa"
    This reverts commit 2bd1896f3331dc11ebde949ed338707b9967a80a.

--- Projektarbete MVC/Projektarbete MVC/Global.asax.cs
+++ Projektarbete MVC/Projektarbete MVC/Global.asax.cs
@@ -1,31 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Projektarbete_MVC.Cart.Models;
-using Projektarbete_MVC.MockData;
-using Projektarbete_MVC.Models;
 
 namespace Projektarbete_MVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static IEnumerable<Product> ProductList;
-
-        public static Cart.Models.CartObject CartObject;
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            ProductList = MockProduct.GetProducts();
-            CartObject = new CartObject
-            {
-                CartItems = new List<CartItem>()
-            };
         }
     }
 }
