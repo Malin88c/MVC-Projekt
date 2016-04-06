c7d2cf4459e684234283d0b7b8efb9d39093af83
Malin Classon <m.classon@hotmail.com>
2016-04-06 21:28:55 +0200

    Created initial migration and updated database.

---
+++ Projektarbete MVC/Projektarbete MVC/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Projektarbete_MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Projektarbete_MVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Projektarbete_MVC.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Projektarbete_MVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
