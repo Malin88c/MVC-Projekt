5b5fbd62b1e4ec4d25e92668d02c7eee06cf4bc3
Malin Classon <m.classon@hotmail.com>
2016-04-06 15:38:07 +0200

    Initial Create

---
+++ Projektarbete MVC/Projektarbete MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projektarbete_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cats()
+        {
+            ViewBag.Message = "Här kan du bläddra bland de katter vi har att erbjuda.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Projektarbete MVC/Projektarbete MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projektarbete_MVC.Startup))]
+namespace Projektarbete_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
